98744525ebe5f1616a8a6fd4835095bcaf759250
EddyShimwa <shedpray16@gmail.com>
2023-06-08 23:32:24 +0200

    modified the sizes

--- main.js
+++ main.js
@@ -15,9 +15,22 @@ const mesh = new THREE.Mesh(Geometry, material);
 //add mesh to scene
 scene.add(mesh);
 
+// sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
+//light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+
 // camera 
 
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 20;
 scene.add(camera);
 
 
@@ -25,5 +38,5 @@ scene.add(camera);
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas })
 
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
