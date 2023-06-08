5577a7df5c7d5996a74f1602fcddb1e9d14bcb7f
EddyShimwa <shedpray16@gmail.com>
2023-06-08 12:55:58 +0200

    add sphere and material

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import * as THREE from 'three';
 
 //Scene
+const scene = new THREE.Scene();
+
+//my sphere
+const sphereGeometry = new THREE.SphereGeometry(3, 64, 64);
+
+//my material
+const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+
+//my mesh
