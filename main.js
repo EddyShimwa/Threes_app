945854d77825ce5d14579c30542b16061ea7ff69
EddyShimwa <shedpray16@gmail.com>
2023-06-12 22:42:22 +0200

    add timeliine magics

--- main.js
+++ main.js
@@ -78,5 +78,10 @@ loop();
 
 // Timeline magic 
 
-const tl = gsap.timeline({ defaults: { duration: 2 } });
+const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0}, { z: 1, x: 1, y: 1})
+tl.fromTo('nav', {y: "-100%"}, {y: "0%"})
+tl.fromTo('title', {opacity: 0}, {opacity: 1})
+
+
+// mouse animation color 
