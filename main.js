56d1157cc208ca57e24259e073b15e2ddc473a84
EddyShimwa <shedpray16@gmail.com>
2023-06-12 22:31:48 +0200

    add the styles for contents

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import './style.css';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Timeline } from 'gsap/gsap-core';
 
 //Scene
 const scene = new THREE.Scene();
@@ -35,6 +36,7 @@ const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 
 camera.position.z = 20;
 scene.add(camera);
 
+
 //renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas })
@@ -42,17 +44,19 @@ const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-
-
-
-
-
-
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
 
 // resize 
 
 window.addEventListener("resize", () => {
     //update sizes
+    controls.update();
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
 
@@ -69,3 +73,8 @@ const loop = () => {
     window.requestAnimationFrame(loop);
 }
 loop();
+
+
+// Timeline magic 
+
+const ti
